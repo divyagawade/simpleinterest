3003f40ebb540259c37316090e3c3485853003e3
divyagawade <102578193+divyagawade@users.noreply.github.com>
2023-09-28 20:38:08 +0530

    Add files via upload

---
+++ threeinteger.java
@@ -0,0 +1,17 @@
+
+public class threeinteger 
+{
+    public static void main(String []args)
+    {
+        int num1=26;
+        int num2=6;
+        int num3=2;
+        if(num1>=num2&&num1>=num3)
+        System.out.println(num1+"is maximum number");
+        else if(num2>=num1&&num2>=num3)
+        System.out.println(num2+"is maximum numbeer");
+        else
+        System.out.println(num3+"is maximun number");
+    }
+    
+}
