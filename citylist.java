d2acd2a9137e1a94c71fd12754f932e429584a6f
divyagawade <102578193+divyagawade@users.noreply.github.com>
2023-09-28 20:37:28 +0530

    Add files via upload

---
+++ citylist.java
@@ -0,0 +1,13 @@
+public class citylist
+{
+    public static void main(String args[])
+    {
+        System.out.println("list of city names:-");
+        for(String city:args)
+        
+        {
+            System.out.println(city);
+        }
+    }
+    
+}
