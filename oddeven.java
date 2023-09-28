df92c848931a993a747c73a1e901b86ecbf7d8cf
divyagawade <102578193+divyagawade@users.noreply.github.com>
2023-09-28 21:15:17 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+public class oddeven
+{
+    public static void main(String[]args)
+    {
+        int num=10;
+        if(num%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is not odd ");
+            
+
+        }
+    }
+
+}
