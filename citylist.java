f09debdd0af75bcddff231398c8b318989ef4df0
divyagawade <102578193+divyagawade@users.noreply.github.com>
2023-09-28 20:41:05 +0530

    Update citylist.java

--- citylist.java
+++ citylist.java
@@ -1,13 +1,22 @@
-public class citylist
-{
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class str{
     public static void main(String args[])
-    {
-        System.out.println("list of city names:-");
-        for(String city:args)
-        
-        {
-            System.out.println(city);
+    throws IOException{
+        //String str;
+        Scanner myobj=new Scanner(System.in);
+        String city[]=new String[10];
+        System.out.println("enter a city 1 to 5:");
+        for(int i=0;i<5;i++){
+            city[i]=myobj.nextLine();
         }
+         for(int i=0;i<5;i++){
+            System.out.println("you enter cites is: "+city[i]+",");
+        }
+        
+        
+
     }
-    
-}
+    }
